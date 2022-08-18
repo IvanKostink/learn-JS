596d882e75372c487996a767cdf33d10c3065d0e
Ivan <110723815+IvanKostink@users.noreply.github.com>
2022-08-18 15:25:36 +0300

    HomeWork #4

--- ls/script1.js
+++ ls/script1.js
@@ -1,74 +1,75 @@
 "use strict";
-let numberOfFilm;
 
-function start () {
-    numberOfFilm = prompt('Сколько фильмовы ты посмотрел?', '');
-
-    while (numberOfFilm == ' ' || numberOfFilm == null || isNaN(numberOfFilm) ) {
-        numberOfFilm = prompt('Сколько фильмовы ты посмотрел?', '');
-    }
-}
-
-start();
 
 const personalMovieDB = {
-    count: numberOfFilm,
+    /* count: numberOfFilm, */
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+    start: function() {
+        personalMovieDB.count = prompt('Сколько фильмовы ты посмотрел?', '');
+    
+        while (personalMovieDB.count == ' ' || personalMovieDB.count == null || isNaN(personalMovieDB.count) ) {
+            personalMovieDB.count = prompt('Сколько фильмовы ты посмотрел?', '');
+        }
+    },
+    rememberMyFilms: function() {
+        for (let i=0; i<2; i++) {
+            let film = prompt('Posledniy Film', '').trim(),
+                rate = prompt('Oceni', '').trim();
+        
+            if(film != null && rate != null && film !='' && rate != '' && film.length < 50 ) {
+                personalMovieDB.movies[film] = rate;
+                console.log('done');
+        
+            } else {
+                console.log('error');
+                i--;
+            }
+        }
+    }, 
+    detectPersonalLevel: function() {
+        if (personalMovieDB.count < 10) {
+            console.log("malo");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log("norm");
+        } else if (personalMovieDB.count >= 30) {
+            console.log("kinoman");
+        } else {
+            console.log("oshibka");
+        }
+    },
+    showMyDB: function (hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    writeYourGenres: function () {
+        for (let i=1; i<=3; i++) {
+            let genre = prompt(`Vash lubimiy zhanr pod nomerom ${i}`);
+            
+            if(genre == null || genre =='') {
+                console.log('oshibka');
+                i--;
+        
+            } else {
+                personalMovieDB.genres[i - 1] = genre;
+            }
+        }
+
+    personalMovieDB.genres.forEach((item, i) => {
+        console.log(`Lyubimiy zhanr ${i + 1} - eto ${item}`);
+    });
+    },
+    toogleVisibleMyDB: function () {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        }   else {personalMovieDB.privat = true;}
+    }
+    
 };
 
-function rememberMyFilms() {
-    for (let i=0; i<2; i++) {
-        let film = prompt('Posledniy Film', ''),
-            rate = prompt('Oceni', '');
-    
-        if(film != null && rate != null && film !='' && rate != '' && film.length < 50 ) {
-            personalMovieDB.movies[film] = rate;
-            console.log('done');
-    
-        } else {
-            console.log('error');
-            i--;
-        }
-    }
-}
-
-rememberMyFilms();
 
 
 
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log("malo");
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log("norm");
-    } else if (personalMovieDB.count >= 30) {
-        console.log("kinoman");
-    } else {
-        console.log("oshibka");
-    }
-}
-
-detectPersonalLevel();
-
-function showMyDB (hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
-
-showMyDB(personalMovieDB.privat);
-
-function writeYourGenres () {
-    for (let i=1; i<=3; i++) {
-        personalMovieDB.genres[i - 1] = prompt(`Vash lubimiy zhanr pod nomerom ${i}`);
-    }
-}
-
-writeYourGenres();
-
-console.log(personalMovieDB);
-
-    
---
+++ ls/script2.js
@@ -0,0 +1,106 @@
+/* const arr = [1, 12, 3, 26, 8];
+arr.sort(compareNum);
+console.log(arr);
+
+function compareNum(a, b) {
+    return a-b;
+}
+
+
+arr.pop();
+arr.push(10);
+
+console.log(arr);
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+for (let value of arr) {
+    console.log(value);
+}
+
+arr.forEach( function (item, i, arr) {
+    console.log(`${i}: ${item} vnutri masiva ${arr}`);
+});
+
+
+const str = prompt() */
+
+/* let a = 5,
+    b = a;
+
+b = b + 5;
+
+console.log(b);
+console.log(a);
+
+const obj =  {
+    a: 5,
+    b: 5
+}; */
+
+/* const copy = obj;
+
+copy.a = 10;
+
+console.log(copy);
+console.log(obj); */
+
+
+/* function copy(mainObj) {
+    let objCopy = {};
+
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+    return objCopy;
+}
+
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
+
+const newObj = copy(numbers);
+
+newObj.a = 10; */
+/* console.log(newObj);
+console.log(numbers); */
+
+
+/* const add = {
+    d: 17,
+    i: 20
+};
+
+console.log(Object.assign(numbers, add)); */
+
+/* const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice;
+
+const video = ['yu', 'vimeo', 'tt'],
+        blogs = ['sa', 'as', 'ewq'],
+        internet = [...video, ...blogs, 'awqwq' ,'fb'];
+
+    console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+const num = [2, 5, 7];
+
+log(...num); */
+
+/* const array = [1, 2];
+
+const nArray = [...array]; */
+
